aa155891b7946defd35d9457faa5b8046933413a
Wesley Matos <wrickee@gmail.com>
2020-02-27 17:39:32 -0300

    made Error401 page

---
+++ src/pages/Error401/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fixed-center text-center">
+    <p>
+      <img
+        src="statics/sad.svg"
+        style="width:30vw;max-width:150px;"
+      >
+    </p>
+    <p class="text-faded">
+      Sorry, you don't have access to this page...<strong>(401)</strong>
+    </p>
+    <q-btn
+      color="secondary"
+      style="width:200px;"
+      to="/"
+      label="Go back"
+    />
+  </div>
+</template>
+
+<script src="./script.js" />
