f47b58b380b66779ec9e875a14996e6438bcdc3c
Wesley Matos <wrickee@gmail.com>
2020-02-28 16:37:14 -0300

    made plugin to validations

---
+++ src/boot/validator.js
@@ -0,0 +1,6 @@
+export default ({ Vue }) => {
+  Vue.prototype.$validations = {
+    required: (val, message = 'Campo obrigatório.') => (val && val.length) || message,
+    email: (val, message = 'Campo precisa ser um e-mail') => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val) || message,
+  };
+};
