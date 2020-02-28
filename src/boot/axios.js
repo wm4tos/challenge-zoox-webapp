3f1d97d34acd66d2b28bc6bcd838a711b2df4d98
Wesley Matos <wrickee@gmail.com>
2020-02-28 13:15:14 -0300

    configure axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,34 @@
-import Vue from 'vue';
 import axios from 'axios';
+import { Loading, Notify } from 'quasar';
 
-Vue.prototype.$axios = axios;
+export default ({ Vue }) => {
+  Vue.prototype.$axios = axios.create({
+    baseURL: `${process.env.API_URL}/api`,
+  });
+
+  axios.interceptors.request.use(
+    (req) => {
+      Loading.show();
+      return req;
+    },
+    (err) => {
+      Loading.hide();
+      Promise.reject(err);
+    },
+  );
+
+  axios.interceptors.response.use(
+    (res) => {
+      Loading.hide();
+      return res;
+    },
+    ({ response: { data } }) => {
+      const { message } = data;
+
+      Notify({
+        message,
+        color: 'red-9',
+      });
+    },
+  );
+};
