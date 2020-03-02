5badff8216d660ea66ca0f31ae57246b2bea341e
Wesley Matos <wrickee@gmail.com>
2020-03-02 12:18:30 -0300

    logout user when status is 401

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import { Loading } from 'quasar';
 
-export default ({ Vue }) => {
+export default ({ Vue, router, store }) => {
   Vue.prototype.$axios = axios.create({
     baseURL: `${process.env.API_URL}/api`,
   });
 
   Vue.prototype.$axios.interceptors.request.use(
-    (req) => {
+    (config) => {
       Loading.show();
 
-      return req;
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${store.getters['user/getToken']}`,
+      };
+
+      return config;
     },
     (err) => {
       Loading.hide();
@@ -25,10 +30,20 @@ export default ({ Vue }) => {
 
       return res.data;
     },
-    (err) => {
+    ({ response }) => {
       Loading.hide();
 
-      return Promise.reject(err.response);
+      switch ((response || {}).status) {
+        case 401:
+          store.dispatch('user/LOGOUT');
+          router.push('/');
+
+          break;
+        default:
+          break;
+      }
+
+      return Promise.reject(response.data);
     },
   );
 };
