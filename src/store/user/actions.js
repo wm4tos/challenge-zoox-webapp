29216e1cf0e52fc7429a77d679fb254c20694964
Wesley Matos <wrickee@gmail.com>
2020-03-02 12:10:07 -0300

    add action to logout user

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -5,3 +5,8 @@ export function SET_USER(ctx, val) {
 export function SET_TOKEN(ctx, val) {
   ctx.commit('SAVE_TOKEN', val);
 }
+
+export function LOGOUT(ctx) {
+  ctx.commit('SAVE_USER', {});
+  ctx.commit('SAVE_TOKEN', '');
+}
