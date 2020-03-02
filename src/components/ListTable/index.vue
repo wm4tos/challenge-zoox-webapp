695b8d75a2ed5ac8af092cabf83ecfd22862d109
Wesley Matos <wrickee@gmail.com>
2020-03-02 16:36:24 -0300

    write component to list items (states or cities)

---
+++ src/components/ListTable/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-table
+    class="list-table"
+    v-bind="{ ...$props, grid: !$q.screen.gt.xs }"
+    v-on="$listeners"
+  >
+    <template v-slot:body-cell-delete="props">
+      <q-td class="flex justify-end">
+        <q-btn
+          icon="delete"
+          color="red-9"
+          @click.stop="$emit('delete', props.row)"
+          flat
+        />
+      </q-td>
+    </template>
+  </q-table>
+</template>
+
+<script src="./script.js" />
+
+<style src="./styles.sass" lang="sass" />
---
+++ src/components/ListTable/script.js
@@ -0,0 +1,6 @@
+import { QTable } from 'quasar';
+
+export default {
+  name: 'ListTable',
+  extends: QTable,
+};
