652487b51a8c732a7b32dec34e6075c7fcaeadd2
Wesley Matos <wrickee@gmail.com>
2020-03-02 18:24:13 -0300

    fix required function on validator plugin

--- src/boot/validator.js
+++ src/boot/validator.js
@@ -1,6 +1,9 @@
 export default ({ Vue }) => {
   Vue.prototype.$validations = {
-    required: (val, message = 'Campo obrigatório.') => (val && !typeof val === 'object' ? val.length : Object.keys(val).length) || message,
+    required: (val, message = 'Campo obrigatório.') => (val && typeof val === 'string'
+      ? val.length
+      : val && Object.keys(val).length
+    ) || message,
     email: (val, message = 'Campo precisa ser um e-mail') => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val) || message,
   };
 };
