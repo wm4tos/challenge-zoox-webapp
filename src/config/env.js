b4f1eb79948bcff1a629d83c1afea95abbaca4b3
Wesley Matos <wrickee@gmail.com>
2020-03-03 15:56:04 -0300

    pass envs manually if "parsed" doesn't exists

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,13 @@ const { config } = require('dotenv');
 const { parsed } = config();
 
 module.exports = () => Object.fromEntries(
-  Object.entries(parsed).map(([key, value]) => (
+  Object.entries(
+    parsed || {
+      API_URL: process.env.API_URL,
+      PORT: process.env.PORT,
+      STORE_KEY: process.env.STORE_KEY,
+    }
+  ).map(([key, value]) => (
     [
       key,
       typeof value === 'string' ? JSON.stringify(value) : value,
