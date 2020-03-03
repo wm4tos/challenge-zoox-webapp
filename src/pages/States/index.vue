3697cee74c43e4acbeaa20facd73d3edd36006ba
Wesley Matos <wrickee@gmail.com>
2020-03-03 15:05:45 -0300

    show states in states page

--- src/pages/States/index.vue
+++ src/pages/States/index.vue
@@ -1,6 +1,39 @@
 <template>
   <q-page>
-    Estados
+    <div class="flex justify-between">
+      <h1 class="text-h3">Estados</h1>
+      <div class="flex items-center">
+        <q-btn
+          color="primary"
+          label="Novo"
+          @click="redirect"
+        />
+      </div>
+    </div>
+
+    <q-form
+      @submit="loadStates"
+      class="row q-gutter-x-lg q-pb-md"
+    >
+      <h2 class="text-h4 col-12"> Busque por </h2>
+      <q-input
+        label="Nome"
+        v-model="filter.name"
+        outlined
+      />
+      <q-btn
+        icon="search"
+        flat
+        type="submit"
+      />
+    </q-form>
+
+    <list-table
+      @row-click="redirect"
+      :data="states"
+      :columns="columns"
+      @delete="deleteState"
+    />
   </q-page>
 </template>
 
--- src/pages/States/script.js
+++ src/pages/States/script.js
@@ -1,3 +1,98 @@
+import ListTable from 'components/ListTable';
+import MixinNotify from 'mixins/notify';
+import HandlerMixin from 'mixins/handlers';
+
 export default {
   name: 'StatesIndexPage',
+  components: {
+    ListTable,
+  },
+  mixins: [
+    MixinNotify,
+    HandlerMixin,
+  ],
+  data() {
+    return {
+      states: [],
+      columns: [
+        {
+          label: 'ID',
+          field: '_id',
+          name: '_id',
+          align: 'left',
+        },
+        {
+          label: 'Nome',
+          field: 'name',
+          name: 'name',
+          align: 'left',
+        },
+        {
+          label: 'UF',
+          field: 'UF',
+          name: 'UF',
+        },
+        {
+          field: 'delete',
+          name: 'delete',
+        },
+      ],
+      options: {
+        states: [],
+      },
+      filter: {
+        _id: null,
+        name: null,
+        state: null,
+      },
+    };
+  },
+  computed: {
+    params() {
+      const { filter } = this;
+
+      return {
+        ...filter,
+        state: filter.state ? filter.state._id : null,
+      };
+    },
+  },
+  methods: {
+    loadStates() {
+      const { params } = this;
+      return this.$axios.get('/states', { params })
+        .then(this.statesSuccess)
+        .catch(this.handlerError);
+    },
+    statesSuccess({ data: states }) {
+      this.states = states;
+    },
+    redirectToCreate() {
+      this.$router.push({ name: 'CreateState' });
+    },
+    redirectToEdit({ _id }) {
+      this.$router.push({ name: 'EditState', params: { _id } });
+    },
+    redirect(event, data) {
+      if (data && data._id) return this.redirectToEdit(data);
+
+      return this.redirectToCreate();
+    },
+    deleteState({ _id }) {
+      return this.$axios.delete(`/states/${_id}`)
+        .then(this.deleteStateSuccess.bind(this, _id))
+        .catch(this.handlerError);
+    },
+    deleteStateSuccess(_id, data) {
+      this.showNotifySuccess(data.message);
+
+      this.states.splice(this.states.findIndex((c) => c._id === _id), 1);
+    },
+    loadData() {
+      this.loadStates();
+    },
+  },
+  created() {
+    this.loadData();
+  },
 };
